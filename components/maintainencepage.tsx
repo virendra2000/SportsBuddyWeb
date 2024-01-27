d0f2bf998e6c63c5d86d04d965d40bcff1e4d376
virendra2000 <vkalwar132000@gmail.com>
2024-01-27 18:08:55 +0530

    Button UI Change in Explore Page

--- components/maintainencepage.tsx
+++ components/maintainencepage.tsx
@@ -13,12 +13,12 @@ const Maintainence = () => {
 
                 <h1 className="text-3xl font-bold text-green-500">SportsBuddy is Under Maintainence !!!</h1>
                 <p className="p-3 font-bold dark:text-white">We are working hard to bring you an Amazing Feature. Stay tuned!</p>
-                <Link href="/" className="w-full flex flex-col items-center justify-center">
+                <Link href="/">
                     <Button 
                     type="button"
                     title="Go to Homepage"
                     icon="/home.gif"
-                    variant="m-2 px-2 py-2 w-[20%] rounded-full border-0 font-bold bg-black dark:bg-white text-white dark:text-black cursor-pointer dark:hover:bg-black dark:hover:text-white shadow-md hover:shadow-black dark:hover:shadow-black"/>
+                    variant="m-2 px-2 py-2 w-full rounded-full border-0 font-bold bg-black dark:bg-white text-white dark:text-black cursor-pointer dark:hover:bg-black dark:hover:text-white shadow-md hover:shadow-black dark:hover:shadow-black"/>
                 </Link>
             </section>
         </>
